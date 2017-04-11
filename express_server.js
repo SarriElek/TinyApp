0c5bd98f13522f9fad7483f768f043c627436028
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-11 23:19:35 +0000

    redirect TinyApp actions

--- express_server.js
+++ express_server.js
@@ -22,7 +22,11 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body);
   let shortURL = generateRandomString();
-  res.send(`OK ${shortURL}`);
+  let longURL = req.body.longURL;
+  if(shortURL && longURL){
+    urlDatabase[shortURL] = longURL;
+  }
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // URL TO CREATE NEW URL
@@ -36,6 +40,16 @@ app.get("/urls/:id", (req, res) => {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+// URL TO REDIRECT SHORT URL TO LONG URL
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  if(longURL){
+    res.redirect(longURL);
+  }
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
