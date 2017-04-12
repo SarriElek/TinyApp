875ad9419d3ac0038429ce4c480e1bf9329e4cac
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-12 20:44:44 +0000

    CRUD implementation in TinyAPP

--- express_server.js
+++ express_server.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
-// set the view engine to ejs
+// CONFIGURATION
 app.set('view engine', 'ejs');
+// MIDDLEWARES
 app.use(bodyParser.urlencoded({extended: true}));
 
 // URLS GET ROUTE HANDLER
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 // URLS POST ROUTE HANDLER
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL;
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
   if(shortURL && longURL){
     urlDatabase[shortURL] = longURL;
   }
@@ -30,26 +30,52 @@ app.post("/urls", (req, res) => {
 });
 
 // URL TO CREATE NEW URL
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 // URL WITH ID ROUTE HANDLER
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+    longURL: urlDatabase[req.params.id]
+  };
+  if(templateVars.longURL){
+    res.render('urls_show', templateVars);
+  }else{
+    res.redirect('/urls');
+  }
+});
+
+// URL WITH ID TO UPDATE ROUTE HANDLER
+app.post('/urls/:id', (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = req.body.longURL;
+  if(longURL){
+    urlDatabase[shortURL] = longURL;
+  }
+  res.redirect('/urls');
+});
+
+// URL WITH ID TO DELETE ROUTE HANDLER
+app.post('/urls/:id/delete', (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+  if(longURL){
+    delete urlDatabase[shortURL];
+  }
+  res.redirect('/urls');
+
 });
 
 // URL TO REDIRECT SHORT URL TO LONG URL
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
   if(longURL){
     res.redirect(longURL);
+  }else{
+     res.redirect('/urls');
   }
-  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
@@ -59,7 +85,7 @@ app.listen(PORT, () => {
 
 function generateRandomString(){
   let randomString = '';
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (var i = 6; i > 0; --i) {
     randomString += possible.charAt(Math.floor(Math.random() * possible.length));
   }
