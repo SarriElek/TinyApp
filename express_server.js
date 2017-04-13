e17cc2b8e1ddcb3290745d1b854e20a804704937
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-13 23:27:44 +0000

    Added content to README

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 3000; // default port 8080
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
