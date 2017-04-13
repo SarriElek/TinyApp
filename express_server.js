0634d207370b962f653ea6c395692d19dafd0b7a
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-13 18:11:12 +0000

    Added registration page

--- express_server.js
+++ express_server.js
@@ -9,6 +9,24 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = [
+{
+  id: 1,
+  username: 'Edurne',
+  password: 'polarbear',
+  email: 'edurne.berastegi@gmail.com'
+},
+{
+  id: 2,
+  username: 'Javier',
+  password: 'grizlybear',
+  email: 'javier@gmail.com'
+}
+];
+
+let lastId = 2;
+
+
 // CONFIGURATION
 app.set('view engine', 'ejs');
 // MIDDLEWARES
@@ -17,12 +35,29 @@ app.use(cookieParser());
 
 // INDEX
 app.get('/', (req, res) => {
-  res.redirect('urls');
+  const user = users.find(user => user.id === Number(req.cookies.id));
+  if(user){
+    res.redirect('urls');
+  }else{
+    res.redirect('login');
+  }
 });
 
 // LOGIN
+app.get('/login', (req, res) => {
+  const user = users.find(user => user.id === Number(req.cookies.id));
+  if(user){
+    res.redirect('/');
+  }else{
+    res.render('login', {user});
+  }
+});
+
 app.post('/login', (req, res) => {
   const userName = req.body.username;
+  // use .find in the array of users to find
+  // the user with that username and compare passwords
+  var user = users.find(user =>  user.username === username);
   res.cookie('username', userName);
   res.redirect('/');
 });
@@ -33,66 +68,138 @@ app.post('/logout', (req, res) => {
   res.redirect('/');
 });
 
+// REGISTRATION
+app.get('/register', (req, res) => {
+    res.render('register', {user : null});
+});
+
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    const id = generateId();
+    const user = {
+      id,
+      email,
+      password,
+      username
+    };
+    users.push(user);
+    res.cookie('id', id);
+    res.redirect('/');
+});
+
 // URLS GET ROUTE HANDLER
 app.get('/urls', (req, res) => {
-  const templateVars = {
-    urls: urlDatabase,
-    username: req.cookies["username"]
-  };
-  res.render('urls_index', templateVars);
+  const user = users.find(user => user.id === Number(req.cookies.id));
+  if(user){
+    const templateVars = {
+      user,
+      urls: urlDatabase
+    };
+    res.render('urls_index', templateVars);
+  }else{
+    const templateVars = {
+       user,
+       errorMessage: 'You must be logged in to see your shortened URLs',
+       showLogin: true
+    };
+    res.status(401).render('error', templateVars);
+  }
+
 });
 
 // URLS POST ROUTE HANDLER
 app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  if(shortURL && longURL){
-    urlDatabase[shortURL] = longURL;
+  if(req.cookies['username']){
+    const shortURL = generateRandomString();
+    const longURL = req.body.longURL;
+    if(shortURL && longURL){
+      urlDatabase[shortURL] = longURL;
+    }
+    res.redirect(`/urls/${shortURL}`);
+  }else{
+    const templateVars = {
+       errorMessage: 'You must be logged in to create new shortened URLs',
+       showLogin: true
+    };
+    res.status(401).render('error', templateVars);
   }
-  res.redirect(`/urls/${shortURL}`);
+
 });
 
 // URL TO CREATE NEW URL
 app.get('/urls/new', (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
+  if(req.cookies['username']){
+    const templateVars = {
+      username: req.cookies["username"]
+    }
+    res.render('urls_new', templateVars);
+  }else{
+    const templateVars = {
+       errorMessage: 'You must be logged in to create new shortened URLs',
+       showLogin: true
+    };
+    res.status(401).render('error', templateVars);
   }
-  res.render('urls_new', templateVars);
 });
 
 // URL WITH ID ROUTE HANDLER
 app.get('/urls/:id', (req, res) => {
-  const templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
-  };
-  if(templateVars.longURL){
-    res.render('urls_show', templateVars);
+  if(req.cookies['username']){
+    const templateVars = {
+      shortURL: req.params.id,
+      longURL: urlDatabase[req.params.id],
+      username: req.cookies["username"]
+    };
+    if(templateVars.longURL){
+      res.render('urls_show', templateVars);
+    }else{
+      res.redirect('/urls');
+    }
   }else{
-    res.redirect('/urls');
+    const templateVars = {
+       errorMessage: 'You must be logged in to update a shortened URLs',
+       showLogin: true
+    };
+    res.status(401).render('error', templateVars);
   }
 });
 
 // URL WITH ID TO UPDATE ROUTE HANDLER
 app.post('/urls/:id', (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = req.body.longURL;
-  if(longURL){
-    urlDatabase[shortURL] = longURL;
+  if(req.cookies['username']){
+    const shortURL = req.params.id;
+    const longURL = req.body.longURL;
+    if(longURL){
+      urlDatabase[shortURL] = longURL;
+    }
+    res.redirect('/urls');
+  }else{
+    const templateVars = {
+       errorMessage: 'You must be logged in to update a shortened URLs',
+       showLogin: true
+    };
+    res.status(401).render('error', templateVars);
   }
-  res.redirect('/urls');
 });
 
 // URL WITH ID TO DELETE ROUTE HANDLER
 app.post('/urls/:id/delete', (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
-  if(longURL){
-    delete urlDatabase[shortURL];
+  if(req.cookies['username']){
+    const shortURL = req.params.id;
+    const longURL = urlDatabase[shortURL];
+    if(longURL){
+      delete urlDatabase[shortURL];
+    }
+    res.redirect('/urls');
+  }else{
+    const templateVars = {
+       errorMessage: 'You must be logged in to delete a shortened URLs',
+       showLogin: false
+    };
+    res.status(404).render('error', templateVars);
   }
-  res.redirect('/urls');
-
 });
 
 // URL TO REDIRECT SHORT URL TO LONG URL
@@ -101,7 +208,11 @@ app.get('/u/:shortURL', (req, res) => {
   if(longURL){
     res.redirect(longURL);
   }else{
-     res.redirect('/urls');
+     const templateVars = {
+       errorMessage: `The URL with id: ${req.params.shortURL} Not Found`,
+       showLogin: false
+     };
+     res.status(404).render('error', templateVars);
   }
 });
 
@@ -117,4 +228,9 @@ function generateRandomString(){
     randomString += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return randomString;
+}
+
+function generateId(){
+  lastId ++
+  return lastId;
 }
